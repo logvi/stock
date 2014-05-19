eea38700bb9a2c25d3580526061e56d68f996ae1
NodeAppCMS <mirenrich@gmail.com>
2014-05-19 19:17:56 +0400

    add files

--- stock/urls.py
+++ stock/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^stock/', include('apps.AppStock.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', login),
 )
